d36ab43e3bf428a37877b49c59ecfed1ce3fe84f
crypto-heimdall <crypto.heimdall@gmail.com>
2019-11-23 20:45:24 +0900

    market update

--- ethereum/contracts/Market.sol
+++ ethereum/contracts/Market.sol
@@ -29,7 +29,7 @@ contract Market {
         RegisteredDiaList[itemCount] = OpenData(itemCount, cut, color, clarity, carat, price, DiaStatus.OffSale);
         itemCount++;
     }
-
+/*
     function getDiamonds () public view returns (OpenData[] memory) {
         OpenData[] memory dias = new OpenData[](itemCount);
         for (uint i = 0; i < itemCount; i++) {
@@ -37,6 +37,28 @@ contract Market {
             dias[i] = dia;
         }
         return dias;
+    }
+*/
+    function getDiamonds () public view returns (string[] memory , string[] memory,string[] memory , string[] memory, uint[] memory ) {
+        
+        string[] memory cuts = new string[](itemCount);
+        string[] memory colors = new string[](itemCount);
+        string[] memory clarity = new string[](itemCount);
+        string[] memory carat = new string[](itemCount);
+        uint[] memory price = new uint[](itemCount);
+        
+        //OpenData[] memory dias = new OpenData[](itemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            OpenData storage dia = RegisteredDiaList[i];
+            cuts[i] = dia.cut;
+            colors[i] = dia.color;
+            clarity[i] = dia.clarity;
+            carat[i] = dia.carat;
+            
+            price[i] = dia.price;
+            
+        }
+        return (cuts, colors, clarity, carat, price);
     }
 
     function transitState (uint itemId, DiaStatus newStatus) public {
